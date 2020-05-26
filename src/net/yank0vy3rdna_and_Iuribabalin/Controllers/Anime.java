b4068be8e2d84313596dd1b4e5202d67bd8c1839
yank0vy3rdna <yankovyerdna@yandex.ru>
2020-05-26 03:17:08 +0300

    Anime

--- src/net/yank0vy3rdna_and_Iuribabalin/Controllers/Anime.java
+++ src/net/yank0vy3rdna_and_Iuribabalin/Controllers/Anime.java
@@ -14,19 +14,28 @@ import java.util.LinkedList;
 public class Anime {
     static final int WINDOW_X = 1000;
     static final int WINDOW_Y = 800;
+
+    void draw_dragon(int x, int y, int size, Color color){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(color);
+        gc.fillRect(x,y,size,size);
+
+    }
+
     @FXML
     private Canvas canvas;
     @FXML
     void initialize() {
-        System.out.println("lol");
         canvas.setHeight(WINDOW_Y);
         canvas.setWidth(WINDOW_X);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
         LinkedList<Color> colors = new LinkedList<>();
         for(StoredType dragon : Main.collectionWorker.collection){
             int color_owner_id = (int)(dragon.getOwner_id() % 7);
-            gc.setFill(Color.rgb(255*(color_owner_id / 4), 255*((color_owner_id / 2) % 2), 255*(color_owner_id % 2)));
-            gc.fillRect(dragon.getCoordinates().getX().intValue(),(int)dragon.getCoordinates().getY(),(int)(dragon.getWeight()/2),(int)(dragon.getWeight()/2));
+            Color color = Color.rgb(255*(color_owner_id / 4), 255*((color_owner_id / 2) % 2), 255*(color_owner_id % 2));
+            int x = dragon.getCoordinates().getX().intValue();
+            int y = (int)dragon.getCoordinates().getY();
+            int size = (int)(dragon.getWeight()/2);
+            draw_dragon(x,y,size,color);
         }
     }
 
